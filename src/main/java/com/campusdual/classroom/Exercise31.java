4aa5b5fe4be0274b0f843685999ad7adde5aa979
Alex Domínguez <doalex7@gmail.com>
2024-11-22 08:45:52 +0100

    Ejercicio completado.

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,21 @@
 package com.campusdual.classroom;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
+        try (Stream<String> stream = Files.lines(Paths.get("src/main/resources/lorem.txt"))) {
+            stream.forEach(System.out::println);
+        } catch (IOException e) {
+            System.out.println("Hubo un error.");
+        }
 
     }
 }
